db2373d31e6febea68259b03fb5b63dd33f75c4e
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2021-02-27 14:15:22 -0800

    Update server.js

--- application/config/server.js
+++ application/config/server.js
@@ -1,8 +1,8 @@
 ({
-  host: '127.0.0.1',
+  host: '0.0.0.0',
   balancer: this.process.env.PORT,
   protocol: 'http',
-  ports: [],
+  ports: [this.process.env.PORT],
   timeout: 5000,
   concurrency: 1000,
   queue: {
