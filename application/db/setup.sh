172dba921ccda45927c6a22ec5b606bd3e709a52
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2021-02-28 01:54:41 -0800

    Update setup.sh

--- application/db/setup.sh
+++ application/db/setup.sh
@@ -1,4 +1,3 @@
 [[ -z "$DATABASE_URL" ]] && export DATABASE_URL='postgresql://postgres@localhost:5432/postgres'
-psql -f install.sql $DATABASE_URL
 psql -f structure.sql $DATABASE_URL
 psql -f data.sql $DATABASE_URL
